a00ab2319bf776587d1ff464173fad18b04907bb
mbrettsc <burakmartin007@outlook.com>
2024-02-24 13:26:58 +0300

    friends added

---
+++ friends/srcs/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import FriendsHandler
+
+urlpatterns = [
+    path('add', FriendsHandler.as_view({'post': 'add'})),
+    path('accept', FriendsHandler.as_view({'post': 'accept'})),
+    path('reject', FriendsHandler.as_view({'delete': 'reject'})),
+    path('request', FriendsHandler.as_view({'get': 'request'})),
+    path('list', FriendsHandler.as_view({'get': 'list'})),
+    path('delete', FriendsHandler.as_view({'delete': 'delete'})),
+]
+
+# POST    /friends/add
+# POST    /friends/accept
+# DELETE  /friends/reject
+# GET     /friends/request
+# GET     /friends/list
+# DELETE  /friends/delete
