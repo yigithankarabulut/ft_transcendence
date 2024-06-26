f9d2410be1adfb427277e6ef832e96356ec2ed33
reepNao <recep.buss@gmail.com>
2024-06-27 00:14:35 +0300

    return2working

--- frontend/src/routes/homepage/HomePage.js
+++ frontend/src/routes/homepage/HomePage.js
@@ -1,16 +1,50 @@
-import { navigateTo } from "./src/utils/navTo.js";
+import { navigateTo } from "../../utils/navTo.js";
+import { toggleHidden,  insertIntoElement } from "../../utils/utils.js";
 
-// Display the navigation bar
+const userDetailUrl = "http://127.0.0.1:8000/user/details";
 document.getElementById('nav-bar').style.display = 'flex';
-
-document.addEventListener("DOMContentLoaded", () => {
-    const playButton = document.querySelector(".button-container a");
-    if (playButton) {
-        playButton.addEventListener("click", (e) => {
-            e.preventDefault();
-            // Start the game logic here
-            console.log("Game is starting...");
-            navigateTo("/quickplay");
-        });
+export async function fetchUserDetails() {
+    const access_token = localStorage.getItem("access_token");
+    if (!access_token) {
+        navigateTo("/login");
+        return;
     }
-});
+    toggleHidden('home-spinner');
+    try {
+        const response = await fetch(userDetailUrl, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${access_token}`,
+            }
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(errorData.error);
+        }
+
+        const data = await response.json();
+        const user = data[0].data[0];
+        const fullname = document.getElementById('fullname')
+        const username = document.getElementById('username');
+        const email = document.getElementById('email');
+
+        insertIntoElement('fullname', user.fullname);
+        insertIntoElement('username', user.username);
+        insertIntoElement('email', user.email);
+        toggleHidden('fullname');
+        toggleHidden('home-spinner');
+    } catch (err) {
+        console.log(err);
+        if (err.error === "Unauthorized") {
+            navigateTo("/login");
+        } else if (err.error === "Token has expired") {
+            navigateTo("/login");
+        } else if (err.error) {
+            console.error("Error: ", err.error);
+        } else {
+            console.error("Error: internal server error");
+        }
+    }
+}
