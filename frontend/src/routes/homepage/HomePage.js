75bb73b13711b06851d4448743d9f5701d23085b
reepNao <recep.buss@gmail.com>
2024-06-28 16:37:21 +0300

    buton-clicked

--- frontend/src/routes/homepage/HomePage.js
+++ frontend/src/routes/homepage/HomePage.js
@@ -21,4 +21,34 @@ if (!access_token) {
     if (quickPlayButton) {
         quickPlayButton.addEventListener("click", handleButtonClick);
     }
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const form = document.querySelector(".requires-validation");
+
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (form.checkValidity()) {
+                const roomLimit = document.querySelector('input[name="room_limit"]').value;
+                const gameScore = document.querySelector('input[name="game_score"]').value;
+                const players = document.querySelector('input[name="players"]').value.split(',').map(player => player.trim());
+
+                const data = {
+                    room_limit: parseInt(roomLimit, 10),
+                    game_score: parseInt(gameScore, 10),
+                    players: players
+                };
+                
+                // JSON verisini console'da görüntüleyin
+                console.log(JSON.stringify(data));
+                // Veriyi localStorage'e kaydedin
+                localStorage.setItem("gameData", JSON.stringify(data));
+                // `game.js` dosyasına yönlendirin
+                navigateTo("/game");
+            }
+
+            form.classList.add('was-validated');
+        }, false);
+    });
 }
