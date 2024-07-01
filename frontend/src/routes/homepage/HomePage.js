5d6c40e0d98ae33d0c69d22671c6a0caa8fad851
reepNao <recep.buss@gmail.com>
2024-07-01 16:11:16 +0300

    some-fixed

--- frontend/src/routes/homepage/HomePage.js
+++ frontend/src/routes/homepage/HomePage.js
@@ -1,12 +1,26 @@
-import { navigateTo } from "../../utils/navTo.js";
-import { toggleHidden } from "../../utils/utils.js";
+const loadScript = (src) => {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.onload = () => resolve();
+        script.onerror = () => reject(new Error(`Script load error for ${src}`));
+        document.head.appendChild(script);
+    });
+};
 
-document.getElementById('nav-bar').style.display = 'flex';
-
-const access_token = localStorage.getItem("access_token");
-if (!access_token) {
-    console.log("No access token found");
-    navigateTo("/login");
-} else {
-    console.log("Access token found");
-}
+Promise.all([
+    loadScript('../../utils/navTo.js?v=1.0'),
+    loadScript('../../utils/utils.js?v=1.0')
+])
+.then(() => {
+    document.getElementById('nav-bar').style.display = 'flex';
+    
+    const access_token = localStorage.getItem("access_token");
+    if (!access_token) {
+        console.log("No access token found");
+        navigateTo("/login");
+    } else {
+        console.log("Access token found");
+    }
+})
+.catch(error => console.error(error));
