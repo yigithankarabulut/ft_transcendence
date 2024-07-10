2913a468029a9271e19c4e09e306edc98a5ff03b
Vahdet Parlak <vahdetparlak@gmail.com>
2024-07-10 18:21:39 +0300

    user_search_prevent_submit

--- frontend/src/routes/users/Users.js
+++ frontend/src/routes/users/Users.js
@@ -9,6 +9,12 @@ export async function fetchUsers() {
         console.log("No access token found");
         navigateTo("/login");
     } else {
+        
+        document.getElementById('search-form').addEventListener("submit", function(event) {
+            event.preventDefault();
+            document.getElementById("search-button").click();
+        });
+
         document.getElementById("search-button").addEventListener("click", async () => {
             const searchValue = document.getElementById("search").value;
             const response = await fetch(searchUrl + "?page=1" + "&limit=5" +"&key=" + searchValue, {
@@ -18,65 +24,67 @@ export async function fetchUsers() {
                     "Authorization": `Bearer ${access_token}`,
                 }
             });
+
             const data = await response.json();
             const users = data[0].data;
-            const tableBody = document.querySelector('.widget-26 tbody');
-            tableBody.innerHTML = ''; // Clear previous content
+            console.log(users);
+            // const tableBody = document.querySelector('.widget-26 tbody');
+            // tableBody.innerHTML = ''; // Clear previous content
 
-            tableBody = document.querySelector('.widget-26 tbody');
+            // tableBody = document.querySelector('.widget-26 tbody');
 
-            users.forEach(user => {
-                const userElement = document.createElement('tr');
-                userElement.innerHTML = `
-                    <td>
-                        <div class="widget-26-job-emp-img">
-                            <img src="${user.avatar}" alt="Company" />
-                        </div>
-                    </td>
-                    <td>
-                        <div class="widget-26-job-title">
-                            <a href="#">${user.job_title}</a>
-                            <p class="m-0"><a href="#" class="employer-name">${user.employer_name}</a> <span class="text-muted time">${user.time}</span></p>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="widget-26-job-info">
-                            <p class="type m-0">${user.job_type}</p>
-                            <p class="text-muted m-0">in <span class="location">${user.location}</span></p>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="widget-26-job-salary">${user.salary}</div>
-                    </td>
-                    <td>
-                        <div class="widget-26-job-category bg-soft-danger">
-                            <i class="indicator bg-danger"></i>
-                            <span>${user.category}</span>
-                        </div>
-                    </td>
-                    <td>
-                        <div class="widget-26-job-starred">
-                            <a href="#">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-width="2"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    class="feather feather-star"
-                                >
-                                    <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
-                                </svg>
-                            </a>
-                        </div>
-                    </td>
-                `;
-                tableBody.appendChild(userElement);
-            });
+            // users.forEach(user => {
+            //     const userElement = document.createElement('tr');
+            //     userElement.innerHTML = `
+            //         <td>
+            //             <div class="widget-26-job-emp-img">
+            //                 <img src="${user.avatar}" alt="Company" />
+            //             </div>
+            //         </td>
+            //         <td>
+            //             <div class="widget-26-job-title">
+            //                 <a href="#">${user.job_title}</a>
+            //                 <p class="m-0"><a href="#" class="employer-name">${user.employer_name}</a> <span class="text-muted time">${user.time}</span></p>
+            //             </div>
+            //         </td>
+            //         <td>
+            //             <div class="widget-26-job-info">
+            //                 <p class="type m-0">${user.job_type}</p>
+            //                 <p class="text-muted m-0">in <span class="location">${user.location}</span></p>
+            //             </div>
+            //         </td>
+            //         <td>
+            //             <div class="widget-26-job-salary">${user.salary}</div>
+            //         </td>
+            //         <td>
+            //             <div class="widget-26-job-category bg-soft-danger">
+            //                 <i class="indicator bg-danger"></i>
+            //                 <span>${user.category}</span>
+            //             </div>
+            //         </td>
+            //         <td>
+            //             <div class="widget-26-job-starred">
+            //                 <a href="#">
+            //                     <svg
+            //                         xmlns="http://www.w3.org/2000/svg"
+            //                         width="24"
+            //                         height="24"
+            //                         viewBox="0 0 24 24"
+            //                         fill="none"
+            //                         stroke="currentColor"
+            //                         stroke-width="2"
+            //                         stroke-linecap="round"
+            //                         stroke-linejoin="round"
+            //                         class="feather feather-star"
+            //                     >
+            //                         <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
+            //                     </svg>
+            //                 </a>
+            //             </div>
+            //         </td>
+            //     `;
+            //     tableBody.appendChild(userElement);
+            // });
         }
         );
     }
