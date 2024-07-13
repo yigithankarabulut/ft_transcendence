ea4b6f0dfd1989babb056765877efb40fe9e3cbb
Recep BATTAL <recep.buss@gmail..com>
2024-07-13 13:50:06 +0300

    1307-1349

--- frontend/src/routes/login/Login.js
+++ frontend/src/routes/login/Login.js
@@ -5,57 +5,56 @@ const loginUrl = "http://127.0.0.1:8000/user/login";
 const IntraOAuthUrl = "http://127.0.0.1:8000/auth/intra"
 
 export async function fetchLogin() {
+    document.getElementById('login-form').addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
 
-document.getElementById('login-form').addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+        if (!email || !password) {
+            insertIntoElement('fields-warning', "Fields shouldn't be empty");
+            return;
+        }
+        toggleHidden('login-form');
+        try {
+            const response = await fetch(loginUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.error || "Internal server error");
+            }
+            const data = await response.json();
+            localStorage.setItem("email", email);
+            navigateTo("/2fa");
+        } catch (err) {
+            insertIntoElement('fields-warning', `Error: ${err.message}`);
+            toggleHidden('login-form');
+        }
+    });
 
-    if (!email || !password) {
-        insertIntoElement('fields-warning', "Fields shouldn't be empty");
-        return;
-    }
-    toggleHidden('login-form');
-    try {
-        const response = await fetch(loginUrl, {
+    document.getElementById('FtButton').addEventListener("click", () => {
+        const response =  fetch(IntraOAuthUrl, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ email, password }),
         });
-        if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData.error || "Internal server error");
-        }
-        const data = await response.json();
-        localStorage.setItem("email", email);
-        navigateTo("/2fa");
-    } catch (err) {
-        insertIntoElement('fields-warning', `Error: ${err.message}`);
-        toggleHidden('login-form');
-    }
-});
-
-document.getElementById('FtButton').addEventListener("click", () => {
-    const response =  fetch(IntraOAuthUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
-    response.then(res => {
-        if (!res.ok) {
-            return res.json().then(err => {
-                throw err;
-            });
-        }
-
-        res.json().then(data => {
-            if (data.url) {
-                window.location.replace(data.url);
+        response.then(res => {
+            if (!res.ok) {
+                return res.json().then(err => {
+                    throw err;
+                });
             }
-        });
-    })
-});
+
+            res.json().then(data => {
+                if (data.url) {
+                    window.location.replace(data.url);
+                }
+            });
+        })
+    });
 }
