02579a79476b8ff3a68c6b7420af4589c98d4283
Vahdet Parlak <vparlak@k2m14s04.42kocaeli.com.tr>
2024-07-15 16:04:35 +0300

    localtournemet-resetpass

--- frontend/src/routes/resetpassword/Resetpassword.js
+++ frontend/src/routes/resetpassword/Resetpassword.js
@@ -1,4 +1,5 @@
 import { navigateTo } from "../../utils/navTo.js";
+import { insertIntoElement, toggleHidden } from "../../utils/utils.js";
 
 const resetUrlBase = "http://localhost:8004/user/pwd/change";
 
@@ -9,13 +10,20 @@ export async function fetchResetpassword() {
 		const urlParams = new URLSearchParams(window.location.search);
 		const uidb64 = urlParams.get("uidb64");
 		const token = urlParams.get("token");
-		const resetUrl = resetUrlBase + "/" + uidb64 + "/" + token + "/";
+		const resetUrl = `${resetUrlBase}/${uidb64}/${token}/`;
 
 		const newPassword = document.getElementById('new-password').value;
 		const confirmPassword = document.getElementById('confirm-password').value;
+		const fields_warning = document.getElementById('fields-warning');
+		const fields_success = document.getElementById('fields-success');
+
+		if (newPassword === '' || confirmPassword === '') {
+			insertIntoElement('fields-warning', "Fields shouldn't be empty");
+			return;
+		}
 
 		if (newPassword !== confirmPassword) {
-			alert('New password and confirmed password do not match');
+			insertIntoElement('fields-warning', 'New password and confirmed password do not match');
 			return;
 		}
 
@@ -29,18 +37,27 @@ export async function fetchResetpassword() {
 				headers: {
 					'Content-Type': 'application/json',
 				},
-				body: JSON.stringify(body)
+				body: JSON.stringify(body),
 			});
 
 			if (!response.ok) {
-				const error = await response.json();
-				throw new Error(error.error);
+				const err = await response.json();
+				throw err;
 			}
 
-			alert('Password has been reset successfully');
-			navigateTo("/login");
-		} catch (error) {
-			alert(error.message);
+			const data = await response.json();
+			setTimeout(() => {
+				navigateTo("/login");
+			}, 2000);
+
+		} catch (err) {
+			if (err.error) {
+				insertIntoElement('fields-warning', "Error: " + err.error);
+
+			} else if (err.new_password) {
+				insertIntoElement('fields-warning', "Password error: " + err.new_password[0]);
+			} 
 		}
 	});
 }
+
